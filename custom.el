2f98dcb9793e1e96f6d3c1886c96e73c0bed29a2
Ketan Agrawal <agrawalk@stanford.edu>
2020-02-23 15:09:07 -0800

    moved customizations to custom.el

---
+++ custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-electric-math (quote ("$" . "$")))
+ '(custom-safe-themes
+   (quote
+    ("de0b7245463d92cba3362ec9fe0142f54d2bf929f971a8cdf33c0bf995250bcf" "c616e584f7268aa3b63d08045a912b50863a34e7ea83e35fcab8537b75741956" "3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" default)))
+ '(org-journal-date-format "%A, %d %B %Y")
+ '(org-journal-dir "~/org/journal/")
+ '(org-journal-find-file (quote find-file))
+ '(org-roam-directory "~/org/")
+ '(package-selected-packages
+   (quote
+    (auctex org-journal evil-org use-package evil-surround magit material-theme airline-themes evil-commentary spaceline))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
