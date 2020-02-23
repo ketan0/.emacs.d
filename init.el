ac8d86d947e0980bc166f3717c21eafe63ed1286
Ketan Agrawal <agrawalk@stanford.edu>
2020-02-22 17:30:07 -0800

    straight for package mgmt, helm, org-roam

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (require 'package)
-;; Any add to list for package-archives (to add marmalade or melpa) goes here
+; ;; Any add to list for package-archives (to add marmalade or melpa) goes here
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")
 			 ("marmalade" . "http://marmalade-repo.org/packages/")
@@ -11,16 +11,32 @@
 (setq package-enable-at-startup nil)
 (package-initialize)
 
+;; BOOTSTRAP STRAIGHT.EL
+(defvar bootstrap-version)
+(let ((bootstrap-file
+        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+      (url-retrieve-synchronously
+        "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+        'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
 ;; USE-PACKAGE CONFIG
 ;; automatically install use-package if on a fresh system
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-when-compile
-  ;; Following line is not needed if use-package.el is in ~/.emacs.d
-  (add-to-list 'load-path "/Users/ketanagrawal/.emacs.d/elpa/")
-  (require 'use-package))
-(setq use-package-always-ensure t)
+; (unless (package-installed-p 'use-package)
+;   (package-refresh-contents)
+;   (package-install 'use-package))
+; (eval-when-compile
+;   ;; Following line is not needed if use-package.el is in ~/.emacs.d
+;   (add-to-list 'load-path "~/.emacs.d/elpa/")
+;   (require 'use-package))
+; ; (setq use-package-always-ensure t)
 
 ;;GENERAL EMACS SETTINGS
 ;; Disable the splash screen (to enable it again, replace the t with 0)
@@ -55,6 +71,21 @@
   "Edit the `user-init-file', in *this* window."
   (interactive)
   (find-file user-init-file))
+
+(defun xah-new-empty-buffer ()
+  "Create a new empty buffer.
+New buffer will be named “untitled” or “untitled<2>”, “untitled<3>”, etc.
+
+It returns the buffer (for elisp programing).
+
+URL `http://ergoemacs.org/emacs/emacs_new_empty_buffer.html'
+Version 2017-11-01"
+  (interactive)
+  (let (($buf (generate-new-buffer "untitled")))
+    (switch-to-buffer-other-window $buf)
+    (funcall initial-major-mode)
+    (setq buffer-offer-save t)
+    $buf))
 
 (when (version<= "26.0.50" emacs-version )
   (global-display-line-numbers-mode))
@@ -98,6 +129,11 @@
   :config
   (setq TeX-auto-save t))
 
+(use-package mac-pseudo-daemon
+  :straight (mac-pseudo-daemon :type git :host github :repo "DarwinAwardWinner/mac-pseudo-daemon")
+  :config
+  (mac-pseudo-daemon-mode t))
+
 (use-package org
   :ensure t
   :config
@@ -112,19 +148,40 @@
   (global-set-key (kbd "C-c a") 'org-agenda)
   (global-set-key (kbd "C-c c") 'org-capture))
 
+(use-package org-roam
+      :after org
+      :hook 
+      (after-init . org-roam-mode)
+      :straight (:host github :repo "jethrokuan/org-roam" :branch "develop")
+      :custom
+      (org-roam-directory "~/org/"))
+
 (use-package org-journal
   :ensure t
   :defer t
   :custom
+  (org-journal-find-file 'find-file)
   (org-journal-dir "~/org/journal/")
   (org-journal-date-format "%A, %d %B %Y"))
+
+(use-package helm
+  :ensure t
+  :config 
+  (helm-mode t))
 
 (use-package evil
   :ensure t
   :init
-  ; (setq evil-want-C-i-jump nil) ;cuz C-i and TAB are same in terminal
+  ;(setq evil-want-C-i-jump nil) ;cuz C-i and TAB are same in terminal
   :config 
+  ;; Make evil-mode up/down operate in screen lines instead of logical lines
+  (define-key evil-motion-state-map "j" 'evil-next-visual-line)
+  (define-key evil-motion-state-map "k" 'evil-previous-visual-line)
+  ;; Also in visual mode
+  (define-key evil-visual-state-map "j" 'evil-next-visual-line)
+  (define-key evil-visual-state-map "k" 'evil-previous-visual-line)
   ; (define-key evil-motion-state-map (kbd "C-c i") 'evil-jump-forward)
+
   (evil-mode t))
 
 (unless (display-graphic-p)
@@ -153,14 +210,23 @@
   :ensure t
   :config
   (evil-leader/set-leader "<SPC>")
-  (evil-leader/set-key
-  "f" 'find-file
-  "i" 'er-find-user-init-file
-  "j" 'org-journal-new-entry
-  "b" 'switch-to-buffer
-  "w" 'save-buffer
-  "t" 'find-todo-file
-  "g" 'magit-status)
+  (evil-leader/set-key ;active in all modes
+    "b" 'switch-to-buffer
+    "f" 'find-file
+    "g" 'magit-status
+    "i" 'er-find-user-init-file
+    "j" 'org-journal-new-entry
+    "o" 'xah-new-empty-buffer
+    "p" 'switch-to-prev-buffer
+    "q" 'delete-other-windows
+    "t" 'find-todo-file
+    "w" 'save-buffer)
+  (evil-leader/set-key-for-mode 'org-mode ;just for org-mode, normal state
+    "n i" 'org-roam-insert
+    "n l" 'org-roam
+    "n f" 'org-roam-find-file
+    "n g" 'org-roam-show-graph)
+  ;; (evil-leader/set-key-for-mode 'org-roam-mode ;just for org-mode, normal state
   (global-evil-leader-mode t))
 
 ;;intialize
