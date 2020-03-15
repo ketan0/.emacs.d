e196b248821264d0565365ab9208a9c84e12d766
Ketan Agrawal <agrawalk@stanford.edu>
2020-03-15 01:48:30 -0400

    Switched everything over to straight.el

--- custom.el
+++ custom.el
@@ -3,20 +3,24 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-electric-math (quote ("$" . "$")))
  '(custom-safe-themes
    (quote
-    ("de0b7245463d92cba3362ec9fe0142f54d2bf929f971a8cdf33c0bf995250bcf" "c616e584f7268aa3b63d08045a912b50863a34e7ea83e35fcab8537b75741956" "3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" default)))
+    ("672bb062b9c92e62d7c370897b131729c3f7fd8e8de71fc00d70c5081c80048c" "55c2069e99ea18e4751bd5331b245a2752a808e91e09ccec16eb25dadbe06354" "3f5f69bfa958dcf04066ab2661eb2698252c0e40b8e61104e3162e341cee1eb9" "c9b89349d269af4ac5d832759df2f142ae50b0bbbabcce9c0dd53f79008443c9" "b89ae2d35d2e18e4286c8be8aaecb41022c1a306070f64a66fd114310ade88aa" "d362eed16f74bfa8e49df0185a9336184d479e120c41837a5e6f020e0336bf7f" "de0b7245463d92cba3362ec9fe0142f54d2bf929f971a8cdf33c0bf995250bcf" "c616e584f7268aa3b63d08045a912b50863a34e7ea83e35fcab8537b75741956" "3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" default)))
+ '(helm-completion-style (quote emacs))
+ '(linum-format "%3i")
  '(org-journal-date-format "%A, %d %B %Y")
  '(org-journal-dir "~/org/journal/")
  '(org-journal-find-file (quote find-file))
  '(org-roam-directory "~/org/")
  '(package-selected-packages
    (quote
-    (auctex org-journal evil-org use-package evil-surround magit material-theme airline-themes evil-commentary spaceline))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+    (auctex org-journal evil-org use-package evil-surround magit material-theme airline-themes evil-commentary spaceline)))
+ '(powerline-color1 "#3d3d68")
+ '(powerline-color2 "#292945"))
+
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(default ((((class color) (min-colors 89)) (:foreground "#333333" :background "#FFFFFF")))))
