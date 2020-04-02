5149e09fb0443cfdc37b6e92c37de4b65f38a163
Ketan Agrawal <agrawalk@stanford.edu>
2020-04-02 00:36:14 -0400

    updates

--- custom.el
+++
@@ -1,9 +0,0 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(helm-completion-style (quote helm-fuzzy))
- '(package-selected-packages
-   (quote
-    (auctex org-journal evil-org use-package evil-surround magit material-theme airline-themes evil-commentary spaceline))))
